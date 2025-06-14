3f988360627147aa4fd2c65a47db360cd916d536
Mark Ma <2402610672@qq.com>
2025-06-14 17:38:05 +0800

    modified:   README.md 	modified:   include/SMU_Lib/functional.h 	modified:   src/SMU_Lib/functional_PB.cpp 	modified:   src/main.cpp

--- src/SMU_Lib/functional_PB.cpp
+++ src/SMU_Lib/functional_PB.cpp
@@ -1,2 +1,16 @@
 #include "SMU_Lib/functional.h"
-//尽请期待
+#include "definer.h"
+#include "robot-config.h"
+
+void Suck_Spin(double v, vex::velocityUnits vu){
+    for(int i = 0; i < Suck_Count; i++){
+        Sucks[i].spin(fwd, v, vu);
+    }
+}
+
+void Suck_Stop(brakeType bt){
+    for(int i = 0; i < Suck_Count; i++){
+        Sucks[i].stop(bt);
+    }
+}
+
