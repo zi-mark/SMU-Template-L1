e1507bb56f78c15a5c75e435f58aa98168d72251
Mark Ma <2402610672@qq.com>
2025-07-05 18:26:41 +0800

    add default value to sucks movement

--- include/SMU_Lib/functional.h
+++ include/SMU_Lib/functional.h
@@ -1,4 +1,4 @@
 #include "robot-config.h"
 
-extern void Suck_Spin(double v, vex::velocityUnits vu);
-extern void Suck_Stop(brakeType bt);
+extern void Suck_Spin(double v, vex::velocityUnits vu = vex::velocityUnits::pct);
+extern void Suck_Stop(brakeType bt = vex::brakeType::hold);
