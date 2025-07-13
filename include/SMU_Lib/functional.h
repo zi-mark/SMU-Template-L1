cf23c7ef2f6a6eab4e2713bbf589de4428e2c5a2
Mark Ma <2402610672@qq.com>
2025-07-13 16:55:58 +0800

    add lift

--- include/SMU_Lib/functional.h
+++ include/SMU_Lib/functional.h
@@ -2,3 +2,7 @@
 
 extern void Suck_Spin(double v, vex::velocityUnits vu = vex::velocityUnits::pct);
 extern void Suck_Stop(brakeType bt = vex::brakeType::hold);
+
+extern void Lift_Spin(double v, vex::velocityUnits vu = vex::velocityUnits::pct);
+extern void Lift_Stop(brakeType bt = brakeType::hold);
+extern void Lift_SpinTo(double target, double v = 90, velocityUnits vu = velocityUnits::pct, bool wait = 1);
--- src/SMU_Lib/functional_PB.cpp
+++ src/SMU_Lib/functional_PB.cpp
@@ -14,3 +14,30 @@ void Suck_Stop(brakeType bt){
     }
 }
 
+void Lift_Spin(double v, velocityUnits vu){
+    for(int i = 0; i < Lift_Count; i++) {
+        Lifts[i].spin(fwd, v, vu);
+    }
+}
+
+void Lift_Stop(brakeType bt){
+    for(int i = 0; i < Lift_Count; i++) {
+        Lifts[i].stop(bt);
+    }
+}
+
+void Lift_SpinTo(double target, double v, velocityUnits vu, bool wait){
+    for(int i = 0; i < Lift_Count; i++) {
+        Lifts[i].spinTo(target, deg, v, vu, 0);
+    }
+    if(wait){
+        wait = 0;
+        while(!wait){
+            for(int i = 0; i < Lift_Count; i++) {
+                wait *= Lifts[i].isDone();
+            }
+        }
+        Lift_Stop();
+    }
+}
+
